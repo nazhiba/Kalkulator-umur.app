a32077381c5e064b22f277747afd1959eebe717a
Nadzib <nadzib22@gmail.com>
2024-03-06 07:24:28 +0700

    kotlin update 6/03/2024

--- app/src/main/java/com/nazhiba/kalkulatorumur/MainActivity.kt
+++ app/src/main/java/com/nazhiba/kalkulatorumur/MainActivity.kt
@@ -7,18 +7,22 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     //
-    var tvselectedDate:TextView? = null
+    private var tvselectedDate:TextView? = null
+    private var tvageinMinutes:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // penggunaan on clik listener
         val button = findViewById<Button>(R.id.button)
-        tvselectedDate = findViewById(R.id.textView)
+        tvselectedDate = findViewById(R.id.tvselecteddate)
+        tvageinMinutes = findViewById(R.id.tvageinMinutes)
         button.setOnClickListener(){
 
             // pemangilan datapicker
@@ -27,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // function untuk datapicker
-    fun datapicker(){
+    private fun datapicker(){
 
         // inisialisasi kalender
         val mykalender = Calendar.getInstance()
@@ -36,16 +40,41 @@ class MainActivity : AppCompatActivity() {
         val hari = mykalender.get(Calendar.DAY_OF_MONTH)
 
         // pemilihan kalender
-        DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener{ view, selectedyear, selectedmonth, selecteddayOfMonth ->
+            DatePickerDialog.OnDateSetListener{ _, selectedyear, selectedmonth, selecteddayOfMonth ->
                 // menampilkan pop-up
                 Toast.makeText(this,"Tahun was ${selectedyear}, Bulan adalah ${selectedmonth+1}, di hari ${selecteddayOfMonth} BtnDatePicker pressed", Toast.LENGTH_LONG).show()
+
+                val selecteddate = "$selecteddayOfMonth/${selectedmonth+1}/$selectedyear"
+                tvselectedDate?.text = selecteddate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val thedate = sdf.parse(selecteddate)
+                thedate?.let {
+                    val selectedDatesInMinutes = thedate.time/60000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time/60000
+
+                        val differenceInMinutes = currentDateInMinutes - selectedDatesInMinutes
+                        tvageinMinutes?.text = differenceInMinutes.toString()
+                    }
+
+                }
+
+
             },
             tahun,
             bulan,
             hari
-        ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+
+
+
 
     }
 }
